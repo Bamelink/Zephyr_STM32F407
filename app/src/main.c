b5a65da865276a0ba282640fdd74e8d8fd8cc927
Jan Breuer <j.breuer@ostfalia.de>
2022-08-08 21:47:42 +0200

    Change to CAN

--- app/src/main.c
+++ app/src/main.c
@@ -3,244 +3,295 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
-#include <errno.h>
-#include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/can.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/byteorder.h>
 
-#include <zephyr/net/socket.h>
+#define RX_THREAD_STACK_SIZE 512
+#define RX_THREAD_PRIORITY 2
+#define STATE_POLL_THREAD_STACK_SIZE 512
+#define STATE_POLL_THREAD_PRIORITY 2
+#define LED_MSG_ID 0x10
+#define COUNTER_MSG_ID 0x12345
+#define SET_LED 1
+#define RESET_LED 0
+#define SLEEP_TIME K_MSEC(250)
 
-#if defined(CONFIG_USERSPACE)
-#include <zephyr/app_memory/app_memdomain.h>
-K_APPMEM_PARTITION_DEFINE(app_partition);
-struct k_mem_domain app_domain;
-#endif
 
-#include "common.h"
+K_THREAD_STACK_DEFINE(rx_thread_stack, RX_THREAD_STACK_SIZE);
+K_THREAD_STACK_DEFINE(poll_state_stack, STATE_POLL_THREAD_STACK_SIZE);
 
-#define APP_BANNER "Run echo client"
 
-#define INVALID_SOCK (-1)
+const struct device *fcan = DEVICE_DT_GET(DT_ALIAS(fcan));
+const struct device *dlcan = DEVICE_DT_GET(DT_ALIAS(dlcan));
 
-const char lorem_ipsum[] =
-	"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque "
-	"sodales lorem lorem, sed congue enim vehicula a. Sed finibus diam sed "
-	"odio ultrices pharetra. Nullam dictum arcu ultricies turpis congue, "
-	"vel venenatis turpis venenatis. Nam tempus arcu eros, ac congue libero "
-	"tristique congue. Proin velit lectus, euismod sit amet quam in, "
-	"maximus condimentum urna. Cras vel erat luctus, mattis orci ut, varius "
-	"urna. Nam eu lobortis velit."
-	"\n"
-	"Nullam sit amet diam vel odio sodales cursus vehicula eu arcu. Proin "
-	"fringilla, enim nec consectetur mollis, lorem orci interdum nisi, "
-	"vitae suscipit nisi mauris eu mi. Proin diam enim, mollis ac rhoncus "
-	"vitae, placerat et eros. Suspendisse convallis, ipsum nec rhoncus "
-	"aliquam, ex augue ultrices nisl, id aliquet mi diam quis ante. "
-	"Pellentesque venenatis ornare ultrices. Quisque et porttitor lectus. "
-	"Ut venenatis nunc et urna imperdiet porttitor non laoreet massa. Donec "
-	"eleifend eros in mi sagittis egestas. Sed et mi nunc. Nunc vulputate, "
-	"mauris non ullamcorper viverra, lorem nulla vulputate diam, et congue "
-	"dui velit non erat. Duis interdum leo et ipsum tempor consequat. In "
-	"faucibus enim quis purus vulputate nullam."
-	"\n";
 
-const int ipsum_len = sizeof(lorem_ipsum) - 1;
+struct k_thread rx_thread_data;
+struct k_thread poll_state_thread_data;
+struct k_work_poll change_led_work;
+struct k_work state_change_work;
+enum can_state current_state;
+struct can_bus_err_cnt current_err_cnt;
 
-APP_DMEM struct configs conf = {
-	.ipv4 = {
-		.proto = "IPv4",
-		.udp.sock = INVALID_SOCK,
-		.tcp.sock = INVALID_SOCK,
-	},
-	.ipv6 = {
-		.proto = "IPv6",
-		.udp.sock = INVALID_SOCK,
-		.tcp.sock = INVALID_SOCK,
-	},
+CAN_MSGQ_DEFINE(change_led_msgq, 2);
+CAN_MSGQ_DEFINE(counter_msgq, 2);
+
+
+static struct k_poll_event change_led_events[1] = {
+	K_POLL_EVENT_STATIC_INITIALIZER(K_POLL_TYPE_MSGQ_DATA_AVAILABLE,
+					K_POLL_MODE_NOTIFY_ONLY,
+					&change_led_msgq, 0)
 };
 
-static APP_BMEM struct pollfd fds[4];
-static APP_BMEM int nfds;
-
-static APP_BMEM bool connected;
-K_SEM_DEFINE(run_app, 0, 1);
-
-static void prepare_fds(void)
+void tx_irq_callback(const struct device *dev, int error, void *arg)
 {
-	if (conf.ipv4.udp.sock >= 0) {
-		fds[nfds].fd = conf.ipv4.udp.sock;
-		fds[nfds].events = POLLIN;
-		nfds++;
-	}
+	char *sender = (char *)arg;
 
-	if (conf.ipv4.tcp.sock >= 0) {
-		fds[nfds].fd = conf.ipv4.tcp.sock;
-		fds[nfds].events = POLLIN;
-		nfds++;
-	}
+	ARG_UNUSED(dev);
 
-	if (conf.ipv6.udp.sock >= 0) {
-		fds[nfds].fd = conf.ipv6.udp.sock;
-		fds[nfds].events = POLLIN;
-		nfds++;
-	}
-
-	if (conf.ipv6.tcp.sock >= 0) {
-		fds[nfds].fd = conf.ipv6.tcp.sock;
-		fds[nfds].events = POLLIN;
-		nfds++;
+	if (error != 0) {
+		printk("Callback! error-code: %d\nSender: %s\n",
+		       error, sender);
 	}
 }
 
-static void wait(void)
+void rx_thread(void *arg1, void *arg2, void *arg3)
 {
-	/* Wait for event on any socket used. Once event occurs,
-	 * we'll check them all.
-	 */
-	if (poll(fds, nfds, -1) < 0) {
-		LOG_ERR("Error in poll:%d", errno);
-	}
-}
-
-static int start_udp_and_tcp(void)
-{
-	int ret;
-
-	LOG_INF("Starting...");
-
-	if (IS_ENABLED(CONFIG_NET_TCP)) {
-		ret = start_tcp();
-		if (ret < 0) {
-			return ret;
-		}
-	}
-
-	if (IS_ENABLED(CONFIG_NET_UDP)) {
-		ret = start_udp();
-		if (ret < 0) {
-			return ret;
-		}
-	}
-
-	prepare_fds();
-
-	return 0;
-}
-
-static int run_udp_and_tcp(void)
-{
-	int ret;
-
-	wait();
-
-	if (IS_ENABLED(CONFIG_NET_TCP)) {
-		ret = process_tcp();
-		if (ret < 0) {
-			return ret;
-		}
-	}
-
-	if (IS_ENABLED(CONFIG_NET_UDP)) {
-		ret = process_udp();
-		if (ret < 0) {
-			return ret;
-		}
-	}
-
-	return 0;
-}
-
-static void stop_udp_and_tcp(void)
-{
-	LOG_INF("Stopping...");
-
-	if (IS_ENABLED(CONFIG_NET_UDP)) {
-		stop_udp();
-	}
-
-	if (IS_ENABLED(CONFIG_NET_TCP)) {
-		stop_tcp();
-	}
-}
-
-static void event_handler(struct net_mgmt_event_callback *cb,
-			  uint32_t mgmt_event, struct net_if *iface)
-{
-	return;
-}
-
-static void init_app(void)
-{
-	LOG_INF(APP_BANNER);
-
-#if defined(CONFIG_USERSPACE)
-	struct k_mem_partition *parts[] = {
-#if Z_LIBC_PARTITION_EXISTS
-		&z_libc_partition,
-#endif
-		&app_partition
+	ARG_UNUSED(arg1);
+	ARG_UNUSED(arg2);
+	ARG_UNUSED(arg3);
+	const struct zcan_filter filter = {
+		.id_type = CAN_EXTENDED_IDENTIFIER,
+		.rtr = CAN_DATAFRAME,
+		.id = COUNTER_MSG_ID,
+		.rtr_mask = 1,
+		.id_mask = CAN_EXT_ID_MASK
 	};
+	struct zcan_frame msg;
+	int filter_id;
 
-	int ret = k_mem_domain_init(&app_domain, ARRAY_SIZE(parts), parts);
+	filter_id = can_add_rx_filter_msgq(dlcan, &counter_msgq, &filter);
+	printk("Counter filter id: %d\n", filter_id);
 
-	__ASSERT(ret == 0, "k_mem_domain_init() failed %d", ret);
-	ARG_UNUSED(ret);
-#endif
+	while (1) {
+		k_msgq_get(&counter_msgq, &msg, K_FOREVER);
 
-	init_vlan();
-}
-
-static int start_client(void)
-{
-	int iterations = 20;
-	int i = 0;
-	int ret;
-
-	while (iterations == 0 || i < iterations) {
-		/* Wait for the connection. */
-		k_sem_take(&run_app, K_FOREVER);
-
-		ret = start_udp_and_tcp();
-
-		while (connected && (ret == 0)) {
-			ret = run_udp_and_tcp();
-
-			if (iterations > 0) {
-				i++;
-				if (i >= iterations) {
-					break;
-
-				}
-			}
+		if (msg.dlc != 2U) {
+			printk("Wrong data length: %u\n", msg.dlc);
+			continue;
 		}
 
-		stop_udp_and_tcp();
+		printk("Counter received: %u\n",
+		       sys_be16_to_cpu(UNALIGNED_GET((uint16_t *)&msg.data)));
+	}
+}
+
+void change_led_work_handler(struct k_work *work)
+{
+	struct zcan_frame frame;
+	int ret;
+
+	while (k_msgq_get(&change_led_msgq, &frame, K_NO_WAIT) == 0) {
+		//if (led.port == NULL) {
+		//	printk("LED %s\n", frame.data[0] == SET_LED ? "ON" : "OFF");
+		//} else {
+		//	gpio_pin_set(led.port, led.pin, frame.data[0] == SET_LED ? 1 : 0);
+		//}
 	}
 
-	return ret;
+	ret = k_work_poll_submit(&change_led_work, change_led_events,
+				 ARRAY_SIZE(change_led_events), K_FOREVER);
+	if (ret != 0) {
+		printk("Failed to resubmit msgq polling: %d", ret);
+	}
 }
+
+char *state_to_str(enum can_state state)
+{
+	switch (state) {
+	case CAN_ERROR_ACTIVE:
+		return "error-active";
+	case CAN_ERROR_WARNING:
+		return "error-warning";
+	case CAN_ERROR_PASSIVE:
+		return "error-passive";
+	case CAN_BUS_OFF:
+		return "bus-off";
+	default:
+		return "unknown";
+	}
+}
+
+void poll_state_thread(void *unused1, void *unused2, void *unused3)
+{
+	struct can_bus_err_cnt err_cnt = {0, 0};
+	struct can_bus_err_cnt err_cnt_prev = {0, 0};
+	enum can_state state_prev = CAN_ERROR_ACTIVE;
+	enum can_state state;
+	int err;
+
+	while (1) {
+		err = can_get_state(dlcan, &state, &err_cnt);
+		if (err != 0) {
+			printk("Failed to get CAN controller state: %d", err);
+			k_sleep(K_MSEC(100));
+			continue;
+		}
+
+		if (err_cnt.tx_err_cnt != err_cnt_prev.tx_err_cnt ||
+		    err_cnt.rx_err_cnt != err_cnt_prev.rx_err_cnt ||
+		    state_prev != state) {
+
+			err_cnt_prev.tx_err_cnt = err_cnt.tx_err_cnt;
+			err_cnt_prev.rx_err_cnt = err_cnt.rx_err_cnt;
+			state_prev = state;
+			printk("state: %s\n"
+			       "rx error count: %d\n"
+			       "tx error count: %d\n",
+			       state_to_str(state),
+			       err_cnt.rx_err_cnt, err_cnt.tx_err_cnt);
+		} else {
+			k_sleep(K_MSEC(100));
+		}
+	}
+}
+
+void state_change_work_handler(struct k_work *work)
+{
+	printk("State Change ISR\nstate: %s\n"
+	       "rx error count: %d\n"
+	       "tx error count: %d\n",
+		state_to_str(current_state),
+		current_err_cnt.rx_err_cnt, current_err_cnt.tx_err_cnt);
+
+#ifndef CONFIG_CAN_AUTO_BUS_OFF_RECOVERY
+	if (current_state == CAN_BUS_OFF) {
+		printk("Recover from bus-off\n");
+
+		if (can_recover(dlcan, K_MSEC(100)) != 0) {
+			printk("Recovery timed out\n");
+		}
+	}
+#endif /* CONFIG_CAN_AUTO_BUS_OFF_RECOVERY */
+}
+
+void state_change_callback(const struct device *dev, enum can_state state,
+			   struct can_bus_err_cnt err_cnt, void *user_data)
+{
+	struct k_work *work = (struct k_work *)user_data;
+
+	ARG_UNUSED(dev);
+
+	current_state = state;
+	current_err_cnt = err_cnt;
+	k_work_submit(work);
+}
+
 
 void main(void)
 {
     LOG_INF("First line of main");
     printk("First line of main\n");
 
-    init_app();
-    k_thread_priority_set(k_current_get(), THREAD_PRIORITY);
+	const struct zcan_filter change_led_filter = {
+		.id_type = CAN_STANDARD_IDENTIFIER,
+		.rtr = CAN_DATAFRAME,
+		.id = LED_MSG_ID,
+		.rtr_mask = 1,
+		.id_mask = CAN_STD_ID_MASK
+	};
+	struct zcan_frame change_led_frame = {
+		.id_type = CAN_STANDARD_IDENTIFIER,
+		.rtr = CAN_DATAFRAME,
+		.id = LED_MSG_ID,
+		.dlc = 1
+	};
+	struct zcan_frame counter_frame = {
+		.id_type = CAN_EXTENDED_IDENTIFIER,
+		.rtr = CAN_DATAFRAME,
+		.id = COUNTER_MSG_ID,
+		.dlc = 2
+	};
+	uint8_t toggle = 1;
+	uint16_t counter = 0;
+	k_tid_t rx_tid, get_state_tid;
+	int ret;
 
-#if defined(CONFIG_USERSPACE)
-	k_thread_access_grant(k_current_get(), &run_app);
-	k_mem_domain_add_thread(&app_domain, k_current_get());
+	if (!device_is_ready(dlcan)) {
+		printk("CAN: Device %s not ready.\n", dlcan->name);
+		return;
+	}
 
-	k_thread_user_mode_enter((k_thread_entry_t)start_client, NULL, NULL,
-				 NULL);
-#else
-	exit(start_client());
-#endif
+	//if (led.port != NULL) {
+		//if (!device_is_ready(led.port)) {
+			//printk("LED: Device %s not ready.\n",
+			//       led.port->name);
+			//return;
+		//}
+		//ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_HIGH);
+		//if (ret < 0) {
+			//printk("Error setting LED pin to output mode [%d]",
+			//       ret);
+			//led.port = NULL;
+		//}
+	//}
 
-    while(1)
-    {
-        LOG_INF("while loop");
-        LOG_ERR("while loop");
-        printk("while loop\n");
-        k_sleep(K_MSEC(1000));
-    }
+	k_work_init(&state_change_work, state_change_work_handler);
+	k_work_poll_init(&change_led_work, change_led_work_handler);
+
+	ret = can_add_rx_filter_msgq(dlcan, &change_led_msgq, &change_led_filter);
+	if (ret == -ENOSPC) {
+		printk("Error, no filter available!\n");
+		return;
+	}
+
+	printk("Change LED filter ID: %d\n", ret);
+
+	ret = k_work_poll_submit(&change_led_work, change_led_events,
+				 ARRAY_SIZE(change_led_events), K_FOREVER);
+	if (ret != 0) {
+		printk("Failed to submit msgq polling: %d", ret);
+		return;
+	}
+
+	rx_tid = k_thread_create(&rx_thread_data, rx_thread_stack,
+				 K_THREAD_STACK_SIZEOF(rx_thread_stack),
+				 rx_thread, NULL, NULL, NULL,
+				 RX_THREAD_PRIORITY, 0, K_NO_WAIT);
+	if (!rx_tid) {
+		printk("ERROR spawning rx thread\n");
+	}
+
+	get_state_tid = k_thread_create(&poll_state_thread_data,
+					poll_state_stack,
+					K_THREAD_STACK_SIZEOF(poll_state_stack),
+					poll_state_thread, NULL, NULL, NULL,
+					STATE_POLL_THREAD_PRIORITY, 0,
+					K_NO_WAIT);
+	if (!get_state_tid) {
+		printk("ERROR spawning poll_state_thread\n");
+	}
+
+	can_set_state_change_callback(dlcan, state_change_callback, &state_change_work);
+
+	printk("Finished init.\n");
+
+	while (1) {
+		change_led_frame.data[0] = toggle++ & 0x01 ? SET_LED : RESET_LED;
+		/* This sending call is none blocking. */
+		can_send(dlcan, &change_led_frame, K_FOREVER,
+			 tx_irq_callback,
+			 "LED change");
+		k_sleep(SLEEP_TIME);
+
+		UNALIGNED_PUT(sys_cpu_to_be16(counter),
+			      (uint16_t *)&counter_frame.data[0]);
+		counter++;
+		/* This sending call is blocking until the message is sent. */
+		can_send(dlcan, &counter_frame, K_MSEC(100), NULL, NULL);
+		k_sleep(SLEEP_TIME);
+	}
 }
