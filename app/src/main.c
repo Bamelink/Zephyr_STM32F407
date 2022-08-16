fdf25914010f39ff844092a4ae06c847ea65117f
Jan Breuer <j.breuer@ostfalia.de>
2022-08-16 17:49:38 +0200

    Change to microros module

--- app/src/main.c
+++ app/src/main.c
@@ -1,297 +1,84 @@
-#include <zephyr/zephyr.h>
+#include <zephyr.h>
+#include <time.h>
+#include <device.h>
+#include <devicetree.h>
+#include <drivers/gpio.h>
 
-#include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <std_msgs/msg/int32.h>
 
-#include <zephyr/kernel.h>
-#include <zephyr/sys/printk.h>
-#include <zephyr/device.h>
-#include <zephyr/drivers/can.h>
-#include <zephyr/drivers/gpio.h>
-#include <zephyr/sys/byteorder.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
 
-#define RX_THREAD_STACK_SIZE 512
-#define RX_THREAD_PRIORITY 2
-#define STATE_POLL_THREAD_STACK_SIZE 512
-#define STATE_POLL_THREAD_PRIORITY 2
-#define LED_MSG_ID 0x10
-#define COUNTER_MSG_ID 0x12345
-#define SET_LED 1
-#define RESET_LED 0
-#define SLEEP_TIME K_MSEC(250)
+#include <rmw_microros/rmw_microros.h>
+#include <microros_transports.h>
 
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);for(;;){};}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
 
-K_THREAD_STACK_DEFINE(rx_thread_stack, RX_THREAD_STACK_SIZE);
-K_THREAD_STACK_DEFINE(poll_state_stack, STATE_POLL_THREAD_STACK_SIZE);
+rcl_publisher_t publisher;
+std_msgs__msg__Int32 msg;
 
-
-const struct device *fcan = DEVICE_DT_GET(DT_ALIAS(fcan));
-const struct device *dlcan = DEVICE_DT_GET(DT_ALIAS(dlcan));
-
-
-struct k_thread rx_thread_data;
-struct k_thread poll_state_thread_data;
-struct k_work_poll change_led_work;
-struct k_work state_change_work;
-enum can_state current_state;
-struct can_bus_err_cnt current_err_cnt;
-
-CAN_MSGQ_DEFINE(change_led_msgq, 2);
-CAN_MSGQ_DEFINE(counter_msgq, 2);
-
-
-static struct k_poll_event change_led_events[1] = {
-	K_POLL_EVENT_STATIC_INITIALIZER(K_POLL_TYPE_MSGQ_DATA_AVAILABLE,
-					K_POLL_MODE_NOTIFY_ONLY,
-					&change_led_msgq, 0)
-};
-
-void tx_irq_callback(const struct device *dev, int error, void *arg)
+void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
 {
-	char *sender = (char *)arg;
-
-	ARG_UNUSED(dev);
-
-	if (error != 0) {
-		printk("Callback! error-code: %d\nSender: %s\n",
-		       error, sender);
+	RCLC_UNUSED(last_call_time);
+	if (timer != NULL) {
+		RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
+		msg.data++;
 	}
 }
-
-void rx_thread(void *arg1, void *arg2, void *arg3)
-{
-	ARG_UNUSED(arg1);
-	ARG_UNUSED(arg2);
-	ARG_UNUSED(arg3);
-	const struct zcan_filter filter = {
-		.id_type = CAN_EXTENDED_IDENTIFIER,
-		.rtr = CAN_DATAFRAME,
-		.id = COUNTER_MSG_ID,
-		.rtr_mask = 1,
-		.id_mask = CAN_EXT_ID_MASK
-	};
-	struct zcan_frame msg;
-	int filter_id;
-
-	filter_id = can_add_rx_filter_msgq(dlcan, &counter_msgq, &filter);
-	printk("Counter filter id: %d\n", filter_id);
-
-	while (1) {
-		k_msgq_get(&counter_msgq, &msg, K_FOREVER);
-
-		if (msg.dlc != 2U) {
-			printk("Wrong data length: %u\n", msg.dlc);
-			continue;
-		}
-
-		printk("Counter received: %u\n",
-		       sys_be16_to_cpu(UNALIGNED_GET((uint16_t *)&msg.data)));
-	}
-}
-
-void change_led_work_handler(struct k_work *work)
-{
-	struct zcan_frame frame;
-	int ret;
-
-	while (k_msgq_get(&change_led_msgq, &frame, K_NO_WAIT) == 0) {
-		//if (led.port == NULL) {
-		//	printk("LED %s\n", frame.data[0] == SET_LED ? "ON" : "OFF");
-		//} else {
-		//	gpio_pin_set(led.port, led.pin, frame.data[0] == SET_LED ? 1 : 0);
-		//}
-	}
-
-	ret = k_work_poll_submit(&change_led_work, change_led_events,
-				 ARRAY_SIZE(change_led_events), K_FOREVER);
-	if (ret != 0) {
-		printk("Failed to resubmit msgq polling: %d", ret);
-	}
-}
-
-char *state_to_str(enum can_state state)
-{
-	switch (state) {
-	case CAN_ERROR_ACTIVE:
-		return "error-active";
-	case CAN_ERROR_WARNING:
-		return "error-warning";
-	case CAN_ERROR_PASSIVE:
-		return "error-passive";
-	case CAN_BUS_OFF:
-		return "bus-off";
-	default:
-		return "unknown";
-	}
-}
-
-void poll_state_thread(void *unused1, void *unused2, void *unused3)
-{
-	struct can_bus_err_cnt err_cnt = {0, 0};
-	struct can_bus_err_cnt err_cnt_prev = {0, 0};
-	enum can_state state_prev = CAN_ERROR_ACTIVE;
-	enum can_state state;
-	int err;
-
-	while (1) {
-		err = can_get_state(dlcan, &state, &err_cnt);
-		if (err != 0) {
-			printk("Failed to get CAN controller state: %d", err);
-			k_sleep(K_MSEC(100));
-			continue;
-		}
-
-		if (err_cnt.tx_err_cnt != err_cnt_prev.tx_err_cnt ||
-		    err_cnt.rx_err_cnt != err_cnt_prev.rx_err_cnt ||
-		    state_prev != state) {
-
-			err_cnt_prev.tx_err_cnt = err_cnt.tx_err_cnt;
-			err_cnt_prev.rx_err_cnt = err_cnt.rx_err_cnt;
-			state_prev = state;
-			printk("state: %s\n"
-			       "rx error count: %d\n"
-			       "tx error count: %d\n",
-			       state_to_str(state),
-			       err_cnt.rx_err_cnt, err_cnt.tx_err_cnt);
-		} else {
-			k_sleep(K_MSEC(100));
-		}
-	}
-}
-
-void state_change_work_handler(struct k_work *work)
-{
-	printk("State Change ISR\nstate: %s\n"
-	       "rx error count: %d\n"
-	       "tx error count: %d\n",
-		state_to_str(current_state),
-		current_err_cnt.rx_err_cnt, current_err_cnt.tx_err_cnt);
-
-#ifndef CONFIG_CAN_AUTO_BUS_OFF_RECOVERY
-	if (current_state == CAN_BUS_OFF) {
-		printk("Recover from bus-off\n");
-
-		if (can_recover(dlcan, K_MSEC(100)) != 0) {
-			printk("Recovery timed out\n");
-		}
-	}
-#endif /* CONFIG_CAN_AUTO_BUS_OFF_RECOVERY */
-}
-
-void state_change_callback(const struct device *dev, enum can_state state,
-			   struct can_bus_err_cnt err_cnt, void *user_data)
-{
-	struct k_work *work = (struct k_work *)user_data;
-
-	ARG_UNUSED(dev);
-
-	current_state = state;
-	current_err_cnt = err_cnt;
-	k_work_submit(work);
-}
-
 
 void main(void)
 {
-    LOG_INF("First line of main");
-    printk("First line of main\n");
+	rmw_uros_set_custom_transport(
+		MICRO_ROS_FRAMING_REQUIRED,
+		(void *) &default_params,
+		zephyr_transport_open,
+		zephyr_transport_close,
+		zephyr_transport_write,
+		zephyr_transport_read
+	);
 
-	const struct zcan_filter change_led_filter = {
-		.id_type = CAN_STANDARD_IDENTIFIER,
-		.rtr = CAN_DATAFRAME,
-		.id = LED_MSG_ID,
-		.rtr_mask = 1,
-		.id_mask = CAN_STD_ID_MASK
-	};
-	struct zcan_frame change_led_frame = {
-		.id_type = CAN_STANDARD_IDENTIFIER,
-		.rtr = CAN_DATAFRAME,
-		.id = LED_MSG_ID,
-		.dlc = 1
-	};
-	struct zcan_frame counter_frame = {
-		.id_type = CAN_EXTENDED_IDENTIFIER,
-		.rtr = CAN_DATAFRAME,
-		.id = COUNTER_MSG_ID,
-		.dlc = 2
-	};
-	uint8_t toggle = 1;
-	uint16_t counter = 0;
-	k_tid_t rx_tid, get_state_tid;
-	int ret;
+	rcl_allocator_t allocator = rcl_get_default_allocator();
+	rclc_support_t support;
 
-	if (!device_is_ready(dlcan)) {
-		printk("CAN: Device %s not ready.\n", dlcan->name);
-		return;
+	// create init_options
+	RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+
+	// create node
+	rcl_node_t node;
+	RCCHECK(rclc_node_init_default(&node, "zephyr_int32_publisher", "", &support));
+
+	// create publisher
+	RCCHECK(rclc_publisher_init_default(
+		&publisher,
+		&node,
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+		"zephyr_int32_publisher"));
+
+	// create timer,
+	rcl_timer_t timer;
+	const unsigned int timer_timeout = 1000;
+	RCCHECK(rclc_timer_init_default(
+		&timer,
+		&support,
+		RCL_MS_TO_NS(timer_timeout),
+		timer_callback));
+
+	// create executor
+	rclc_executor_t executor;
+	RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+	RCCHECK(rclc_executor_add_timer(&executor, &timer));
+
+	msg.data = 0;
+
+	while(1){
+		rclc_executor_spin_some(&executor, 100);
+		usleep(100000);
 	}
 
-	//if (led.port != NULL) {
-		//if (!device_is_ready(led.port)) {
-			//printk("LED: Device %s not ready.\n",
-			//       led.port->name);
-			//return;
-		//}
-		//ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_HIGH);
-		//if (ret < 0) {
-			//printk("Error setting LED pin to output mode [%d]",
-			//       ret);
-			//led.port = NULL;
-		//}
-	//}
-
-	k_work_init(&state_change_work, state_change_work_handler);
-	k_work_poll_init(&change_led_work, change_led_work_handler);
-
-	ret = can_add_rx_filter_msgq(dlcan, &change_led_msgq, &change_led_filter);
-	if (ret == -ENOSPC) {
-		printk("Error, no filter available!\n");
-		return;
-	}
-
-	printk("Change LED filter ID: %d\n", ret);
-
-	ret = k_work_poll_submit(&change_led_work, change_led_events,
-				 ARRAY_SIZE(change_led_events), K_FOREVER);
-	if (ret != 0) {
-		printk("Failed to submit msgq polling: %d", ret);
-		return;
-	}
-
-	rx_tid = k_thread_create(&rx_thread_data, rx_thread_stack,
-				 K_THREAD_STACK_SIZEOF(rx_thread_stack),
-				 rx_thread, NULL, NULL, NULL,
-				 RX_THREAD_PRIORITY, 0, K_NO_WAIT);
-	if (!rx_tid) {
-		printk("ERROR spawning rx thread\n");
-	}
-
-	get_state_tid = k_thread_create(&poll_state_thread_data,
-					poll_state_stack,
-					K_THREAD_STACK_SIZEOF(poll_state_stack),
-					poll_state_thread, NULL, NULL, NULL,
-					STATE_POLL_THREAD_PRIORITY, 0,
-					K_NO_WAIT);
-	if (!get_state_tid) {
-		printk("ERROR spawning poll_state_thread\n");
-	}
-
-	can_set_state_change_callback(dlcan, state_change_callback, &state_change_work);
-
-	printk("Finished init.\n");
-
-	while (1) {
-		change_led_frame.data[0] = toggle++ & 0x01 ? SET_LED : RESET_LED;
-		/* This sending call is none blocking. */
-		can_send(dlcan, &change_led_frame, K_FOREVER,
-			 tx_irq_callback,
-			 "LED change");
-		k_sleep(SLEEP_TIME);
-
-		UNALIGNED_PUT(sys_cpu_to_be16(counter),
-			      (uint16_t *)&counter_frame.data[0]);
-		counter++;
-		/* This sending call is blocking until the message is sent. */
-		can_send(dlcan, &counter_frame, K_MSEC(100), NULL, NULL);
-		k_sleep(SLEEP_TIME);
-	}
+	// free resources
+	RCCHECK(rcl_publisher_fini(&publisher, &node))
+	RCCHECK(rcl_node_fini(&node))
 }
